d351d2789ca7db66d9fb74b30d0603263a371449
Ilya Nikitin <f1uctus@mail.ru>
2022-03-28 16:59:34 +0300

    `test_select_unique_in.py`: Improve assertions

--- tests/test_select_unique_in.py
+++ tests/test_select_unique_in.py
@@ -3,16 +3,21 @@ import altpacom.altpacom as apc
 
 def test_select_unique_in(responses):
     p10 = responses["p10"]["packages"]
+    p10_names = {p["name"] for p in p10}
+
     sisyphus = responses["sisyphus"]["packages"]
+    sisyphus_names = {p["name"] for p in sisyphus}
 
     unique_in_p10 = apc.select_unique_in(
         "p10",
         {"p10": p10, "sisyphus": sisyphus},
     )
-    assert len(unique_in_p10) == 1482
+    for p in unique_in_p10:
+        assert p["name"] not in sisyphus_names
 
     unique_in_sisyphus = apc.select_unique_in(
         "sisyphus",
         {"p10": p10, "sisyphus": sisyphus},
     )
-    assert len(unique_in_sisyphus) == 1412
+    for p in unique_in_sisyphus:
+        assert p["name"] not in p10_names
